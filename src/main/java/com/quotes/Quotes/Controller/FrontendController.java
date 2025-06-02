87c4f8ee2ed317287dc2d79f7083edcf6f9f6de4
Esteban-RG <ricardoespace@gmail.com>
2025-06-02 01:30:16 -0600

    Se conecto el manejo de rutas react con spring

---
+++ src/main/java/com/quotes/Quotes/Controller/FrontendController.java
@@ -0,0 +1,15 @@
+package com.quotes.Quotes.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class FrontendController {
+
+    @RequestMapping(value = {"/", "/about", "/notfound"})
+    public String index() {
+        return "forward:/index.html";
+    }
+    
+}
