85715e0cb6f448ab6042e004501ee661d396faea
Esteban-RG <ricardoespace@gmail.com>
2025-06-02 01:30:16 -0600

    Se conecto el manejo de rutas react con spring

--- frontend/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import ReactDOM  from "react-dom"
-import {Home} from "./views/home"
-import { createRoot } from "react-dom/client"
-
-const home = document.getElementById("home")
-const root = createRoot(home)
-root.render(<Home />)
